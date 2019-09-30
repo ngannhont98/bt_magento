06ba56eb71ed8f16e8b01d73383986f3f1f6fc6a
ngannhont98 <nganphan.300798@gmail.com>
2019-09-30 18:18:10 +0700

    updating collection

--- app/etc/config.php
+++ app/etc/config.php
@@ -212,6 +212,8 @@ return [
         'Klarna_Kp' => 1,
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
+        'Smartosc_Article' => 1,
+        'Smartosc_HelloWorld' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1
     ]
