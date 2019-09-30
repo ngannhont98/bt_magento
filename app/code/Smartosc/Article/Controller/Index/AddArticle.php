5452972fb6924b6f9acdf91e324aa8c24194935d
ngannhont98 <nganphan.300798@gmail.com>
2019-09-30 18:21:24 +0700

    updating collection

---
+++ app/code/Smartosc/Article/Controller/Index/AddArticle.php
@@ -0,0 +1,26 @@
+<?php
+namespace Smartosc\Article\Controller\Index;
+
+use Magento\Framework\App\Action\Context;
+use \Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class DisplayArticle
+ * @package Smartosc\Article\Controller\Index
+ */
+class AddArticle extends \Magento\Framework\App\Action\Action
+
+{
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
